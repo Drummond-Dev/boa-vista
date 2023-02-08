5cf38670a9bdc4e2da57cb2a57ac7a7d172881f5
Drummond-Dev <drummond.daniel@gmail.com>
2023-02-08 12:23:37 -0300

    Instalação Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
@@ -20,5 +21,6 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
+        require('flowbite/plugin'),
     ],
 };
